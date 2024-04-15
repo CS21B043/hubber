3c8efbbdc0c16b05aa6de5ed4cd55f17e893e77a
CS21B043 <110807261+CS21B043@users.noreply.github.com>
2024-04-15 17:13:02 +0530

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -1,3 +1,14 @@
 def hello_world():
   print("Dhananjayan is confused")
   return 14
+
+
+import torchvision.models as models
+
+model = models.vgg16(weights='IMAGENET1K_V1')
+torch.save(model.state_dict(), 'model_weights.pth')
+
+
+def get_model():
+  print("hi")
+  return model
